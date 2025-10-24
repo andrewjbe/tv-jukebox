3d97f480715eebf06328f60620ec7490006522f3
andrewjbe <56839927+andrewjbe@users.noreply.github.com>
2025-10-24 15:17:56 -0500

    fix pathing again

--- tvjuke.py
+++ tvjuke.py
@@ -7,8 +7,8 @@ import threading
 from evdev import InputDevice, categorize, ecodes
 
 # --- CONFIGURATION ---
-WELCOME_DIR = "./welcome-videos"
-SHOWS_DIR = "./shows"
+WELCOME_DIR = "~/tv-jukebox/welcome-videos"
+SHOWS_DIR = "~/tv-jukebox/shows"
 INPUT_DEVICE_PATH = "/dev/input/event0"  # Adjust as needed
 
 KEY_MAP = {
